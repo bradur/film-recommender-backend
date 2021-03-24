217e8c4e25e9b96f8848d2dde0aaeaffb406bbff
Juho Saarelainen <bradurdung@gmail.com>
2021-03-24 21:30:03 +0200

    Add imdb pageIds

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express"
 import cors from 'cors'
 import routes from './src/routes/index.js'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
-const port = 80
+const argv = yargs(hideBin(process.argv))
+  .option('port', {
+    alias: 'p',
+    description: 'Express server port',
+    type: 'number'
+  }).argv
+const port = argv.port ? argv.port : 80
 
 const app = express()
 app.use(cors())
 
 app.use('/api', routes())
-app.listen(process.env.PORT || port)
+app.listen(port)
--- src/data/mockIMDBData.js
+++ src/data/mockIMDBData.js
@@ -4,43 +4,50 @@ const data = {
       genres: ["drama", "scifi"],
       name: "Arrival",
       rating: 7.9,
-      releaseDate: "2016-11-11"
+      releaseDate: "2016-11-11",
+      pageId: "tt2543164"
     },
     {
       genres: ["action", "thriller"],
       name: "Die Hard",
       rating: 8.2,
-      releaseDate: "1998-06-20"
+      releaseDate: "1998-06-20",
+      pageId: "tt0095016"
     },
     {
       genres: ["action", "adventure", "scifi"],
       name: "Predator",
       rating: 7.8,
-      releaseDate: "1987-06-12"
+      releaseDate: "1987-06-12",
+      pageId: "tt0093773"
     },
     {
       genres: ["drama", "romance"],
       name: "Little Women",
       rating: 7.8,
-      releaseDate: "2019-12-25"
+      releaseDate: "2019-12-25",
+      pageId: "tt3281548"
     },
     {
       genres: ["comedy"],
       name: "Airplane!",
       rating: 7.7,
-      releaseDate: "1980-06-02"
+      releaseDate: "1980-06-02",
+      pageId: "tt0080339"
     },
     {
       genres: ["animation", "adventure", "comedy"],
       name: "Toy Story",
       rating: 8.3,
-      releaseDate: "1995-11-22"
+      releaseDate: "1995-11-22",
+      pageId: "tt0114709"
     },
     {
       genres: ["animation", "family", "fantasy"],
       name: "My Neighbor Totoro",
       rating: 8.2,
-      releaseDate: "1990-07-13"
+      releaseDate: "1990-07-13",
+      pageId: "tt0096283"
     },
   ]
 }
