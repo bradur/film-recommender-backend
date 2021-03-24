957f8b151f62104957963d2a7a75aa53bac15022
Juho Saarelainen <bradurdung@gmail.com>
2021-03-25 00:11:47 +0200

    Netflix data

--- src/data/mockIMDBData.js
+++ src/data/mockIMDBData.js
@@ -5,49 +5,56 @@ const data = {
       name: "Arrival",
       rating: 7.9,
       releaseDate: "2016-11-11",
-      pageId: "tt2543164"
+      pageId: "tt2543164",
+      netflix: false
     },
     {
       genres: ["action", "thriller"],
       name: "Die Hard",
       rating: 8.2,
       releaseDate: "1998-06-20",
-      pageId: "tt0095016"
+      pageId: "tt0095016",
+      netflix: true
     },
     {
       genres: ["action", "adventure", "scifi"],
       name: "Predator",
       rating: 7.8,
       releaseDate: "1987-06-12",
-      pageId: "tt0093773"
+      pageId: "tt0093773",
+      netflix: true
     },
     {
       genres: ["drama", "romance"],
       name: "Little Women",
       rating: 7.8,
       releaseDate: "2019-12-25",
-      pageId: "tt3281548"
+      pageId: "tt3281548",
+      netflix: true
     },
     {
       genres: ["comedy"],
       name: "Airplane!",
       rating: 7.7,
       releaseDate: "1980-06-02",
-      pageId: "tt0080339"
+      pageId: "tt0080339",
+      netflix: false
     },
     {
       genres: ["animation", "adventure", "comedy"],
       name: "Toy Story",
       rating: 8.3,
       releaseDate: "1995-11-22",
-      pageId: "tt0114709"
+      pageId: "tt0114709",
+      netflix: true
     },
     {
       genres: ["animation", "family", "fantasy"],
       name: "My Neighbor Totoro",
       rating: 8.2,
       releaseDate: "1990-07-13",
-      pageId: "tt0096283"
+      pageId: "tt0096283",
+      netflix: true
     },
   ]
 }
